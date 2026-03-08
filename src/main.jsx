dc3e27c42889ab68a9f3a7d9be5b3bbbe7fb3349
NLoveDesign <ar@nlove.nl>
2026-03-09 00:08:04 +0100

    main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NLoveLandingPage from "./NLoveLandingPage.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <NLoveLandingPage />
+  </React.StrictMode>
+);
